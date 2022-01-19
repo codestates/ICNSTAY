6f988aae65fe5d5c7672fbcdd977ae7745172b69
Mubarmig <87626133+Mubarmig@users.noreply.github.com>
2022-01-19 21:49:34 +0900

    CREATED: userinfo (#41)
    Co-authored-by: tiahwang44 <tiahwang44@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Accommodation from './pages/Accommodation';
 import Home from './pages/Home';
+import Mypage from './pages/Mypage';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
@@ -11,6 +12,7 @@ function App() {
       <Header isLoggedIn={true} />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route path="/userinfo" element={<Mypage />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/:id" element={<Accommodation />}></Route>
         <Route exact path="/signin" element={<SignIn />}></Route>
