5a78dffb63ece0a62e3bdcb6cfb01992f833f7e5
Mubarmig <throughworld97@gmail.com>
2022-01-19 20:51:02 +0900

    CREATED: signin

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Header isLoggedIn={true} />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+      <Routes>
+        <Route exact path="/signin" element={<SignIn />}></Route>
       </Routes>
     </BrowserRouter>
   );
