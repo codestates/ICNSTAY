9f67a8bbfae36f7aea09a980b26144e8b19ecff4
tiahwang44 <tiahwang44@gmail.com>
2022-01-19 16:42:52 +0900

    CREATED: userinfo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 
 import Home from './pages/Home';
+import Mypage from './pages/Mypage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header isLoggedIn={true} />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route path="/userinfo" element={<Mypage />}></Route>
       </Routes>
     </BrowserRouter>
   );
