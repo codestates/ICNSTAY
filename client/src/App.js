7712696c2dbad72c1e1287c4601f95ae4676c38b
seanswim <swim1720@gmail.com>
2022-01-19 17:36:23 +0900

    CREATED: user signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-
 import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header isLoggedIn={true} />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
