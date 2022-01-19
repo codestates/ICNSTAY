6660901a6f6ad25de0897ce4bb4acfac4b6d4d1a
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-01-19 23:38:42 +0900

    CREATED: signin-server

--- server/router/index.js
+++ server/router/index.js
@@ -6,12 +6,12 @@ rounter.post( '/signin', user.signin );
 rounter.post( '/signup', user.signup );
 rounter.post( '/signout', user.signout );
 rounter.get( '/userinfo/:id', user.userinfo.get );
-rounter.patch( '/userinfo/:id', user.userinfo.post );
+rounter.post( '/userinfo/:id', user.userinfo.post );
 rounter.delete( '/userinfo/:id', user.userinfo.delete );
 rounter.get( '/biddingList', user.biddingList );
 
 rounter.get( '/', accommodation.list );
 rounter.get( '/:id', accommodation.detail );
-rounter.post( '/:id', accommodation.book );
+rounter.post( '/:id', accommodation.book ); // 추후 수정 필요: 코딩 위치가 user.router 위로 올라가면 경로 꼬임
 
 module.exports = rounter;
