bc213b6ecba7f52e6ee5c6d65e024bd47f223a84
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-01-20 14:38:10 +0900

    CREATED: get userinfo

--- server/controller/user/userInfo.js
+++ server/controller/user/userInfo.js
@@ -1,11 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { user } = require('../../models')
+
 module.exports = {
-  get: (req, res) => {
-    res.status(200).json('userInfo get');
+  get: async ( req, res ) => {
+    if ( req.cookies.accessToken ) {
+      const { id } = jwt.verify(req.cookies.accessToken, process.env.ACCESS_SECRET)
+      const userFinder = await user.findOne({
+        where: { id }
+      })
+      if ( !userFinder ) {
+        res.status(404).json({ message: 'email not exist', data: null })
+      }else {
+        const { password, createdAt, updatedAt, ...userInfo } = userFinder.dataValues;
+        res.status(200).json(userInfo);
+      }
+    } else {
+      res.status(401).json({ message: 'invaild access token' })
+    }
   },
-  post: (req, res) => {
+  post: ( req, res ) => {
     res.status(200).json('userInfo post');
   },
-  delete: (req, res) => {
+  delete: ( req, res ) => {
     res.status(200).json('userInfo delete');
   }
 }
