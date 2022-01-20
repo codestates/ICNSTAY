30714533a1d8bd134bc41a9322ddda822e62b784
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-01-20 16:26:48 +0900

    CREATED: delete userinfo

--- server/controller/user/userInfo.js
+++ server/controller/user/userInfo.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const { user } = require('../../models');
+
 module.exports = {
   get: (req, res) => {
     res.status(200).json('userInfo get');
@@ -5,7 +8,16 @@ module.exports = {
   post: (req, res) => {
     res.status(200).json('userInfo post');
   },
-  delete: (req, res) => {
-    res.status(200).json('userInfo delete');
+  delete: async (req, res) => {
+    if ( req.cookies.accessToken ) {
+      const tokenDecoer = jwt.verify(req.cookies.accessToken, process.env.ACCESS_SECRET);
+      const { iat, exp, ...userInfo } = tokenDecoer;
+      await user.destroy({
+        where: userInfo
+      });
+      res.status(200).json({ message: "successfully deleted" })
+    }else {
+      res.status(401).json({ message: 'invalid access token'})
+    }
   }
 }
