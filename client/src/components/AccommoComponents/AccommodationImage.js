83ea498b0df25d3d3e238c37197a7791d04c5ee3
seanswim <swim1720@gmail.com>
2022-01-21 17:23:16 +0900

    ADDED: backup save

--- client/src/components/AccommoComponents/AccommodationImage.js
+++ client/src/components/AccommoComponents/AccommodationImage.js
@@ -1,16 +1,36 @@
 import styled from 'styled-components';
 import testImage from '../../data/githubIcon.png';
 import AccommodationThumb from './AccommodationThumb';
+import React, { useEffect, useState, useRef, forwardRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+
 
 
 const MainContainer = styled.div`
   padding: 2rem 10rem 2rem 0rem;
 `;
 
+const Wrapper = styled.div`
+  padding: 1rem;
+  display: flex;
+  align-items: center;  
+  > span {
+    padding-left: 2em;
+    padding-right: 2em;
+    cursor: pointer;
+  }
+`;
+
 const AccommodationImage = () => {
   return (
     <MainContainer>
-      <img src={testImage}></img>
+      <Wrapper>
+        <span><FontAwesomeIcon icon={faAngleLeft} size='4x'/></span>
+        <img src={testImage}></img>
+        <span><FontAwesomeIcon icon={faAngleRight} size='4x'/></span>
+      </Wrapper>
       <AccommodationThumb />
     </MainContainer>
   );
