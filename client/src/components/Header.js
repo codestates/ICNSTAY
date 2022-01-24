bbf7124c27b4ce72a8f3f65a05a9ffce7a7ad619
Tia Hwang <90431155+tiatiahwang@users.noreply.github.com>
2022-01-24 12:00:44 +0900

    Revert "CREATED: signIn Auth"
    This reverts commit cc7124cdf630ceb1fec666934655541a6b02a670.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -33,21 +33,21 @@ const StyledLink = styled(Link)`
   all: unset;
 `;
 
-const Header = ({ isLogIn, handleSignOut }) => {
-  console.log(isLogIn);
+const Header = ({ isLoggedIn }) => {
   return (
     <HeaderContainer>
       <StyledLink to="/">
         <Logo src={logo} />
       </StyledLink>
+
       <ButtonContainer>
-        {isLogIn ? (
+        {isLoggedIn ? (
           <>
-            <StyledLink to="/userinfo">
+            <StyledLink to="/userInfo">
               <Button>MY PAGE</Button>
             </StyledLink>
-            <StyledLink to="/">
-              <Button onClick={handleSignOut}>LOG OUT</Button>
+            <StyledLink to="/signout">
+              <Button>LOG OUT</Button>
             </StyledLink>
           </>
         ) : (
