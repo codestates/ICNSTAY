cd70f9195323574e48f6d52f52c6f54e50e54f46
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-01-24 15:23:00 +0900

    FIXED: code fixed

--- server/controller/user/biddingList.js
+++ server/controller/user/biddingList.js
@@ -3,20 +3,21 @@ const { user, accommodation, user_accommodation } = require('../../models');
 module.exports = async (req, res) => {
   const { id } = req.params;
   // console.log(id);
-  const finder = await user_accommodation.findAll({
-    where: { userId: id },
-    attributes: { exclude: ['createdAt', 'updatedAt']},
-    include: {
-      model: accommodation,
-      // through: { attributes: [] },
-      attributes: ['name'],
-    }
-  });
-  // console.log(finder.map(list => list.dataValues.accommodation.name)) => 숙박시설 name 조회
-  if (finder) {
-    const list = find.dataValues;
+  try{
+    const finder = await user_accommodation.findAll({
+      where: { userId: id },
+      attributes: { exclude: ['createdAt', 'updatedAt']},
+      include: {
+        model: accommodation,
+        // through: { attributes: [] },
+        attributes: ['name'],
+      }
+    });
+    // console.log(finder.map(list => list.dataValues.accommodation.name)) => 숙박시설 name 조회
+    // console.log(finder);
+    const list = finder.map(list => list.dataValues);
     res.status(200).json(list)
-  }else {
+  }catch(err) {
     res.status(404).json({ message: 'list not exist' });
   }
 }
