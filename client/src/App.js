330febd69e31b9a3e49a7c077424eed104719677
tiahwang44 <tiahwang44@gmail.com>
2022-01-24 13:23:14 +0900

    ADDED: keep signIn state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { useCookies } from 'react-cookie';
 import axios from 'axios';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -12,23 +13,36 @@ import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   const [isLogIn, setIsLogIn] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')) || null);
   const [visitedPage, setVisitedPage] = useState('/'); // 방문한 페이지 저장하는 스택
-  const isAuthenticated = async () => {
-    const res = await axios.get('https://localhost:4000/userinfo');
-    console.log(res);
-    const data = res.data;
-    if (data) {
-      const { id, email, mobile, username } = data;
-      setUser({ id, email, mobile, username });
-      setIsLogIn(true);
-    } else {
-      setUser(null);
+  // const [token, setToken, removeToken] = useCookies(['signInToken']);
+
+  const getUser = async () => {
+    try {
+      const res = await axios.get('https://localhost:4000/userinfo');
+      const userInfo = res.data;
+      if (userInfo) {
+        const { id, email, mobile, username } = userInfo;
+        setUser({ id, email, mobile, username });
+      } else {
+        setUser(null);
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  const handleResponseSuccess = () => {
+  // console.log('App.js : ', user);
+
+  const isAuthenticated = async () => {
+    getUser();
+  };
+
+  const handleResponseSuccess = (accessToken) => {
     isAuthenticated();
+    setIsLogIn(true);
+    localStorage.setItem('token', accessToken);
+    // setToken('signInToken', accessToken);
   };
 
   const handleSignOut = async () => {
@@ -36,11 +50,31 @@ function App() {
     if (signOutRequest.status === 205) {
       setUser(null);
       setIsLogIn(false);
+      localStorage.clear();
+      // removeToken('signInToken');
+      // sessionStorage.removeItem('userInfo');
     }
   };
+
   useEffect(() => {
     console.log('최근 방문한 페이지:', visitedPage);
   }, [visitedPage]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLogIn(true);
+      setUser(user);
+    } else {
+      setIsLogIn(false);
+    }
+  }, []);
+
+  // useEffect(
+  //   () => isLogIn && localStorage.setItem('isLoggedIn', JSON.stringify(isLogIn)),
+  //   [isLogIn]
+  // );
+
+  useEffect(() => user && localStorage.setItem('user', JSON.stringify(user)), [user]);
 
   return (
     <BrowserRouter>
@@ -51,14 +85,17 @@ function App() {
         <Route
           exact
           path="/signin"
-          element={<SignIn handleResponseSuccess={handleResponseSuccess} visitedPage={visitedPage} />}
+          element={
+            <SignIn handleResponseSuccess={handleResponseSuccess} visitedPage={visitedPage} />
+          }
         ></Route>
-        <Route path="/signup" element={<SignUp />}></Route> 
-
+        <Route path="/signup" element={<SignUp />}></Route>
 
         <Route
           path="/userinfo"
-          element={<Mypage setIsLogIn={setIsLogIn} userInfo={user} />}
+          element={
+            <Mypage setIsLogIn={setIsLogIn} user={user} setUser={setUser} getUser={getUser} />
+          }
         ></Route>
 
         <Route path="/biddinglist" element={<BiddingList />}></Route>
