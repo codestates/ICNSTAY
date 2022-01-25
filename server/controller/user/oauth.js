aa5c86ee50de23a0d2233967e0424fc4e4bf48c6
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-01-25 14:30:54 +0900

    FIXED: social login cookie setting

--- server/controller/user/oauth.js
+++ server/controller/user/oauth.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const qs = require('querystring');
 const { user } = require('../../models');
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   signin: async (req, res) => {
@@ -20,7 +21,7 @@ module.exports = {
       const tokenReciever = await axios.post('https://kauth.kakao.com/oauth/token', body, headers)
       const {access_token, refresh_token} = tokenReciever.data
   
-      res.cookie('refreshToken', refresh_token, { sameSite: 'None', secure: true, httpOnly: true })
+      res.cookie('refresh_token', refresh_token, { sameSite: 'None', secure: true, httpOnly: true })
   
       const userInfoReciver = await axios.get("https://kapi.kakao.com/v2/user/me", {
         body: {
@@ -37,7 +38,15 @@ module.exports = {
         defaults: { social: 'kakao' },
         attributes: {exclude: ['password', 'createdAt', 'updatedAt']}
       })
-      
+      const userInfo = userFinder.dataValues
+      const refreshToken = jwt.sign(userInfo, process.env.REFRESH_SECRET, {
+        expiresIn: '6h'
+      })
+      res.cookie('refreshToken', refreshToken, {
+        sameSite: 'None',
+        secure: true,
+        httpOnly: true,
+      })
       res.status(200).json({access_token, userFinder});
     }catch (e) {
       res.status(205).json({ message: "server error" })
