1abdbf589db7068dbbebd2f1b60ddf8b5d058e38
SEB_Full_35_허진혁 <91868151+Jin-hyeok2@users.noreply.github.com>
2022-01-27 12:06:29 +0900

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -6,27 +6,24 @@ module.exports = {
     username: 'root',
     password: '27035182',
     database: process.env.DATABASE_NAME || 'ICNSTAY',
-    host: '127.0.0.1',
-    // port: process.env.DATABASE_PORT,
-    // host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql"
   },
   test: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME || 'ICNSTAY',
-    host: '127.0.0.1',
-    // port: process.env.DATABASE_PORT,
-    // host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql"
   },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME || 'ICNSTAY',
-    host: '127.0.0.1',
-    // host: process.env.DATABASE_HOST,
-    // port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "mysql"
   }
 }
